98db4bab4b1cd021f88685365a4140e310e0bf04
Benjamin Kircher <bkircher@0xadd.de>
2020-12-30 11:27:32 +0100

    Slap a grid on it
    Move away from using window coordinates directly. Define a 10x10 grid
    (the "arena") and position and size systems that map that grid to window
    coordinates.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# crazytanks
+# Crazy Tanks
 
 Links:
+
 - https://github.com/FSMaxB/bevy-pong-clone
 - https://www.youtube.com/channel/UCVs1tUPOooBJcA8mt6coQQQ
 - https://crates.io/crates/bevy
 - https://rust-gamedev.github.io
+- https://github.com/marcusbuffett/bevy_snake
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use bevy::prelude::*;
 
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+
 #[derive(Bundle)]
 struct Tank {}
 
 struct Materials {
     body: Handle<ColorMaterial>,
+}
+
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
+#[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
+struct Position {
+    x: i32,
+    y: i32,
 }
 
 fn spawn_tank(commands: &mut Commands, materials: Res<Materials>) {
@@ -14,26 +37,53 @@ fn spawn_tank(commands: &mut Commands, materials: Res<Materials>) {
             sprite: Sprite::new(Vec2::new(30.0, 30.0)),
             ..Default::default()
         })
-        .with(Tank {});
+        .with(Tank {})
+        .with(Position { x: 3, y: 3 })
+        .with(Size::square(0.8));
 }
 
 fn tank_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut tank_positions: Query<&mut Transform, With<Tank>>,
+    mut tank_positions: Query<&mut Position, With<Tank>>,
 ) {
-    for mut transform in tank_positions.iter_mut() {
+    for mut pos in tank_positions.iter_mut() {
         if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 3.0;
+            pos.x -= 1;
         }
         if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 3.0;
+            pos.x += 1;
         }
         if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 3.0;
+            pos.y += 1;
         }
         if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 3.0;
+            pos.y -= 1;
         }
+    }
+}
+
+fn size_scaling(windows: Res<Windows>, mut query: Query<(&Size, &mut Sprite)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut sprite) in query.iter_mut() {
+        sprite.size = Vec2::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+        );
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut query: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let title_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.0) + (title_size / 2.0)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in query.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
     }
 }
 
@@ -47,10 +97,19 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
 
 fn main() {
     App::build()
+        .add_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_resource(WindowDescriptor {
+            title: "Crazy Tanks!".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..Default::default()
+        })
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_tank.system()))
         .add_system(bevy::input::system::exit_on_esc_system.system())
         .add_system(tank_movement.system())
+        .add_system(position_translation.system())
+        .add_system(size_scaling.system())
         .add_plugins(DefaultPlugins)
         .run();
 }
